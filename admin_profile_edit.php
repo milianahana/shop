453ed2dad7cb87e3ae3a2310c0fbae9151774454
milianahana <hf134497@gmail.com>
2025-09-02 12:43:29 +0530

    profile editing for for admin

---
+++ admin_profile_edit.php
@@ -0,0 +1,66 @@
+<!doctype html>
+<html>
+<head>
+<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<title>Untitled Document</title>
+	<link rel="stylesheet" href="css/grid_style.css">
+	<link rel="stylesheet" href="css/index_style.css">
+	<script type="text/javascript" language="javascript" src="js/jquery.min.js"></script>
+</head>
+
+<body>
+        <div class="col-12 admin_profile" id="profile" style="display: none;">
+            <div class="procon">
+                <div class="col-12 admin_profile_ribbon">
+                </div>
+                <div class="col-12 admin_profile_align">
+                    <div class="col-8 admin_profile_picture">
+                        <div class="col-2"><img src="image/profiles_pictures/default_profile.png" alt=""></div>
+                        <div class="col-10">
+                            <p>djhfdui</p>
+                        </div>
+                    </div>
+                    <div class="col-4 admin_profile_edit"><a href="#">edit</a></div>
+                </div>
+                <div class="col-12">
+                    <div class="col-6">
+                        <h4>Full Name</h4>
+                        <p class="profile_para">kvkfvjkf</p>
+                    </div>
+                    <div class="col-6">
+                        <h4>User Name</h4>
+                        <p class="profile_para">kvkfvjkf</p>
+                    </div>
+                </div>
+                <div class="col-12">
+                    <div class="col-6">
+                        <h4>Gender</h4>
+                        <p class="profile_para">kvkfvjkf</p>
+                    </div>
+                    <div class="col-6">
+                        <h4>Country</h4>
+                        <p class="profile_para">kvkfvjkf</p>
+                    </div>
+                </div>
+                <div class="col-12">
+                    <div class="col-6">
+                        <h4>Language</h4>
+                        <p class="profile_para">kvkfvjkf</p>
+                    </div>
+                    <div class="col-6">
+                        <h4>Time Zone</h4>
+                        <p class="profile_para">kvkfvjkf</p>
+                    </div>
+                </div>
+                <h3 style="padding-left: 25px; margin-top:5px;">My Email Address</h3>
+                <div class="col-12">
+                    <div class="email_scn"><img src="image/facebook(2).png" alt="">hendy@gmail.com</div>
+                </div>
+                <div class="col-12">
+                    <a href="#">+ Add Email Address</a>
+                </div>
+            </div>
+        </div>
+</body>
+</html>
