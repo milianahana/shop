78af8b37275f3ae75d407cbb7d31822511e456bc
milianahana <hf134497@gmail.com>
2025-09-19 14:23:54 +0530

    extraimagepage

--- product_images_addForm.php
+++ product_images_addForm.php
@@ -28,28 +28,39 @@
          <a href="admin_home.php">&#x2190; back</a>
      </div>
      <div class="container-fluid extraImages">
+         <?php
+
+            $table_products = mysqli_fetch_array($select_displayed_items_statement2);
+
+            $p_id = $table_products['displayed_item_id'];
+            $p_name = $table_products['displayed_item_name'];
+            $p_image = $table_products['displayed_item_image'];
+            $p_price = $table_products['displayed_item_rate'];
+            $p_quandity = $table_products['displayed_item_quandity'];
+            $p_category = $table_products['displayed_item_category'];
+
+            ?>
          <div class="coi-12">
-             <?php
-             
-                $table_products = mysqli_fetch_array($select_displayed_items_statement2);
-
-                $p_id = $table_products['displayed_item_id'];
-                $p_name = $table_products['displayed_item_name'];
-                $p_image = $table_products['displayed_item_image'];
-
-                ?>
-
-             <h1><?php echo $p_name;?></h1>
-             <img src="image/item_images/<?php echo $p_image;?>" alt="">
-             <div class="col-12 admin_top_contents">
+             <div class="col-3">
+                 <h2>product details</h2>
+                 <img src="image/item_images/<?php echo $p_image; ?>" alt="image" class="extraImage_head">
+             </div>
+             <div class="col-9">
                  <form action="product_extra_details_form_action.php" class="extraDetails_form" method="post" enctype="multipart/form-data" onSubmit="return validate()">
-                     <input type="file" name="item" class="item_image" id="item_image" onSubmit="return validate()">
-                     <input placeholder="ProductId" type="text" class="name" name="item_id" id="item_id" value="<?php echo $product_id; ?>" onSubmit="return validate()">
-                     <input placeholder="Color" type="text" class="name" name="item_color" id="item_color" onSubmit="return validate()">
-                     <input placeholder="ColorCode" type="text" class="category" name="item_colorid" id="item_colorid" onSubmit="return validate()">
-                     <input placeholder="Size" type="text" class="price" name="item_size" id="item_size" onSubmit="return validate()"><br>
-                     <input type="submit" name="add" value="Add" class="addDetails">
+                     <h1><?php echo $p_name; ?></h1>
+                     <p><?php echo $p_price; ?></p>
+                     <p><?php echo $p_category; ?></p>
+                     <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. 
+                        Lorem Ipsum has been<br> the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley <br>of type and scrambled it to make a type specimen book</p>
                  </form>
+             </div>
+             <div class="col-12 image_add_contents">
+                 <table class="image_add_table">
+                     <tr>
+                         <td style="width:10%;"><img src="image/Terracotta-green-use.png" alt="" width="100%"></td>
+                         <td style="padding:2%;">jsdhfuj</td>
+                     </tr>
+                 </table>
              </div>
          </div>
      </div>
